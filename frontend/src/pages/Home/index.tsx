c21c6a41a84ceb8c8f0c194c5fd1101557d30b86
Rafael Rodrigues <rafaelrodriguesmirandadiniz@gmail.com>
2025-02-17 14:51:25 -0300

    first commit

---
+++ frontend/src/pages/Home/index.tsx
@@ -0,0 +1,17 @@
+
+export function Home() {
+
+    return (
+        <div className="w-full h-screen flex justify-center items-center bg-cyan-500 ">
+            <div className="p-8 bg-cyan-900 rounded flex flex-col" >
+                <input 
+                    type="text" 
+                    placeholder="enter the country's name" 
+                    className="outline-0 text-white"
+                />
+
+                <h1>teste</h1>
+            </div>
+        </div>
+    )
+}
