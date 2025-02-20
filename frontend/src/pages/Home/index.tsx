6e81c4dcf1f407d79784ae1b949f2a4e2c0b9570
Rafael Rodrigues <rafaelrodriguesmirandadiniz@gmail.com>
2025-02-20 14:53:42 -0300

    change: code legibility, color pallete

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -13,6 +13,8 @@ export function Home() {
 
     const [weatherData, setWeatherData] = useState<WeatherApiResponseProps>()
     const [isShowing, setIsShowing] = useState(false)
+    const [isLoading, setIsLoading] = useState(false);
+    const [hasError, setHasError] = useState(false);
     const [inputCityNameValue, setInputCityNameValue] = useState('');
 
     const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
@@ -26,37 +28,73 @@ export function Home() {
     }
 
     const handleClick = async () => {
-        const response: AxiosResponse<WeatherApiResponseProps> = await weather.get('weather', {
-            params: {
-                q: inputCityNameValue,
-                appid: apiKey
-            }
-        })
+        setIsLoading(true);
 
-        if (response) {
-            setWeatherData(response.data);
+        try{
+            const response: AxiosResponse<WeatherApiResponseProps> = await weather.get('weather', {
+                params: {
+                    q: inputCityNameValue,
+                    appid: apiKey
+                }
+            });
+    
+            if (response.data) {
+                setWeatherData(response.data);
+                setIsShowing(true);
+                setIsLoading(false);
+                setHasError(false);
+            }
+        }catch(error) {
+            console.log("Deu erro: ", error)
+            
+            setHasError(true);
+            setIsShowing(false);
+            setIsLoading(false);
         }
 
-        setIsShowing(true);
+    }
+
+    const getWeatherIcon = () => {
+        if (weatherData?.weather[0]) {
+            switch(weatherData.weather[0].main) {
+                case "Clouds":
+                    return cloudsIcon;
+                case "Clear":
+                    return sunIcon
+                case "Thunderstorm":
+                    return stormIcon
+                case "Drizzle":
+                    return cloudsLowRainIcon
+                case "Rain":
+                    return rainIcon         
+            }
+        }
+
+        return ""
     }
 
     return (
-        <div className="w-full h-screen flex justify-center items-center bg-cyan-500 ">
-            <div className="min-w-80 p-8 pb-20 bg-cyan-900 rounded flex flex-col items-center" >
+        <div className="w-full h-screen flex justify-center items-center bg-violet-600">
+            <div 
+                className="min-w-80 h-auto p-8 pb-20 bg-violet-500 rounded 
+                flex flex-col items-center shadow-black " 
+            >
                 <h1 className="text-center text-white text-3xl">Weather</h1>
 
                 <input 
                     type="text" 
                     placeholder="Enter city's name" 
                     onChange={(e) => setInputCityNameValue(e.target.value)}
-                    className="w-full outline-0 text-white px-4 py-2 bg-cyan-700 rounded mt-8"
+                    className="w-full outline-0 text-black placeholder:text-black px-4 py-2 rounded mt-8 bg-violet-200"
                 />
 
                 <button 
                     onClick={handleClick}
-                    className="px-4 py-2 bg-cyan-700 rounded mt-6 w-1/2"
+                    disabled={isLoading}
+                    className="px-4 py-2 bg-violet-600 text-violet-200 rounded 
+                    mt-6 w-1/2 hover:cursor-pointer hover:bg-violet-700 disabled:cursor-not-allowed"
                 >   
-                    Search
+                    {isLoading ? "Searching city..." : "Search"}
                 </button>
 
                 {isShowing ? (
@@ -66,32 +104,26 @@ export function Home() {
                         <div>
                             {}
                             <img 
-                                src={weatherData?.weather[0].main == "Clouds" ? cloudsIcon :
-                                    weatherData?.weather[0].main == "Clear" ? sunIcon :
-                                    weatherData?.weather[0].main == "Thunderstorm" ? stormIcon : 
-                                    weatherData?.weather[0].main == "Drizzle" ? cloudsLowRainIcon : 
-                                    weatherData?.weather[0].main == "Rain" ? rainIcon : 
-                                    "Undefined"
-                                } 
+                                src={getWeatherIcon()}
                                 alt="teste" 
                                 className="w-24 h-24"
                             />
                         </div>
 
                         <div className="mt-6">
-                            <span className="block text-center text-4xl text-white">
+                            <span className="block text-center text-4xl text-violet-200">
                                 {convertKelvinToCelsius(weatherData?.main.temp)}°C
                             </span> 
 
-                            <h2 className="text-center text-white mt-2">
+                            <h2 className="text-center text-violet-200 mt-2 capitalize">
                                 {weatherData?.name} <br/>
-                                Tempo claro
+                                {weatherData?.weather[0].description}
                             </h2>
                         </div>
 
                     </div>
 
-                ) : null}
+                ) : hasError ? <h1 className="text-rose-600 text-xl mt-6">City not found</h1> : null}
             </div>
         </div>
     )
--- frontend/src/services/api/weather.tsx
+++ frontend/src/services/api/weather.tsx
@@ -1,6 +1,5 @@
 import axios, { AxiosInstance } from "axios";
 
-
 export const weather: AxiosInstance = axios.create({
     baseURL: 'https://api.openweathermap.org/data/2.5/'
 })
